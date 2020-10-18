686f25ab022e50d1abded6bf8638efec07dae63d
derese getachew <deresegetachew@gmail.com>
2020-10-18 10:49:26 +0300

    extending value objects to create new once and value objects for product

--- src/entities-usingValueObject-try2.ts
+++ src/entities-usingValueObject-try2.ts
@@ -24,7 +24,6 @@ abstract class ValueObject implements IValueObject<any> {
   abstract isValid(): boolean;
   copy?: (() => any) | undefined;
   equals(val: ValueObject): boolean {
-    console.log('???', this._value, val.value);
     return this._value === val.value;
   }
 
@@ -35,7 +34,6 @@ abstract class ValueObject implements IValueObject<any> {
 }
 
 //value Object
-//using static factory method
 class ProductId extends ValueObject {
   // this is forcing typescript to kind of exact much ProductId Class , if we move this to VAlueObject it cant differentiate the different
   // valueObject Classes we are creating things like productId, Name ..
@@ -77,6 +75,84 @@ class VendorId extends ValueObject {
   }
 }
 
+class Money extends ValueObject {
+  private readonly type: string;
+  constructor(amount: number) {
+    super(amount);
+    this.isValid();
+    Object.setPrototypeOf(this, Money.prototype); //carefully when copying and pasting change the class from ProductId to your new Class
+    this.type = this.constructor.name; // basically setting the className to our type property.
+  }
+
+  isValid(): boolean {
+    if (this.value == undefined || this.value == null)
+      throw new Error('Invalid Money');
+    else return true;
+  }
+
+  format(locale: string, currency: string) {
+    return new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency: currency
+    }).format(this.value);
+  }
+
+  addMoney(amount: number): Money {
+    return new Money(amount + this.value);
+  }
+
+  subtractMoney(amount: number): Money {
+    return new Money(this.value - amount);
+  }
+}
+
+class Price extends Money {
+  isValid(): boolean {
+    if (this.value < 0) {
+      throw new Error('Price cannot be negative');
+    }
+    return true;
+  }
+}
+
+class ProductTitle extends ValueObject {
+  private readonly type: string;
+  constructor(title: string) {
+    super(title);
+    this.isValid();
+    Object.setPrototypeOf(this, ProductTitle.prototype); //carefully when copying and pasting change the class from ProductId to your new Class
+    this.type = this.constructor.name; // basically setting the className to our type property.
+  }
+
+  isValid(): boolean {
+    if (this.value == undefined || this.value == null || this.value.length == 0)
+      throw new Error('Product Title can not be empty');
+    else if (this.value.length > 100)
+      throw new Error('Title can not be longer than 100 characters');
+    else return true;
+  }
+}
+
+class ProductDescription extends ValueObject {
+  private readonly type: string;
+  constructor(description: string) {
+    super(description);
+    this.isValid();
+    Object.setPrototypeOf(this, ProductDescription.prototype); //carefully when copying and pasting change the class from ProductId to your new Class
+    this.type = this.constructor.name; // basically setting the className to our type property.
+  }
+
+  isValid(): boolean {
+    if (this.value == undefined || this.value == null || this.value.length == 0)
+      throw new Error('Product ProductDescription can not be empty');
+    else if (this.value.length > 1000)
+      throw new Error(
+        'ProductDescription can not be longer than 1000 characters'
+      );
+    else return true;
+  }
+}
+
 //Product Entity
 class Product {
   //Our entity has no more check for the produceId, since, by receiving argument of type ProductId we guarantee that the value is valid.
@@ -100,22 +176,22 @@ class Product {
 
   //transfer Ubiquitous language to code; we have defined the behaviors of our entity
   //transferred  words from our command sticky note to methods.
-  public setTitle = function (title: string): void {
+  public setTitle = function (title: ProductTitle): void {
     this.#_title = title;
   };
-  public updateDescription = function (description: string): void {
+  public setDescription = function (description: ProductDescription): void {
     this.#_description = description;
   };
-  public updatePrice = function (price: number): void {
+  public setPrice = function (price: Price): void {
     this.#_price = price;
   };
   public setProductGroup = function (groupId: string): void {
     this.#_groupdId = groupId;
   };
 
-  #_title: string;
-  #_description: string;
-  #_price: number;
+  #_title: ProductTitle;
+  #_description: ProductDescription;
+  #_price: Price;
   #_groupdId: string;
   #_vendorId: VendorId;
 }
@@ -136,7 +212,22 @@ const pr2 = new Product(productIdx, vendorIdx);
 //check if the value objects are equal by value
 console.log(productId.equals(productIdx)); //will output true; our value objects are equal by their value.
 
-//entity level equality based on identity is not yet assured
-console.log(pr1 == pr2); // will output false
+pr1.setDescription(new ProductDescription('Iphone x 8GB RAM, 512 GB Storage'));
+pr1.setPrice(new Price(50));
+pr1.setTitle(new ProductTitle('Iphone X'));
+
+// price extends Money
+
+//let p0 = new Price(-200); //this will throw an error as price can not be negative
+
+const p1 = new Price(50);
+console.log('??', p1.value, p1.format('en-US', 'EUR'));
+
+const dollar1 = new Money(1);
+const dollar2 = new Money(2);
+
+const banknote3 = new Money(3);
+
+console.log(dollar1.addMoney(dollar2.value).equals(banknote3));
 
 export {};
